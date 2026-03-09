d65eab94ba5d2c15c27e3656fcc7f1027519d56a
sangeetharr085-crypto <sangeetharr085@gmail.com>
2026-03-09 18:16:21 +0530

    Add simple calculator using switch-case
    Implement a simple calculator using switch-case for basic operations.

---
+++ Switch .c
@@ -0,0 +1,32 @@
+simple calculator*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char op;
+   int first,second;
+   clrscr();
+   printf("enter the operator(+,-,*,/):);
+   scanf("%c",&op);
+   printf("enter two operands:");
+   scanf("%d %d",&first,&second);
+   switch(op)
+   {
+     case '+':
+     printf("%d + %d = %d",first,second,first + second);
+     break;
+     case'-':
+     printf("%d - %d = %d",first,second,first-second);
+     break;
+     case'*':
+     printf("%d * %d = %d",first,second,first * second);
+     break;
+     case'/':
+     printf("%d / %d = %d",first,second,first / second);
+     break;
+     default:
+     printf("error ! operation is not correct");
+     }
+     getch();
+     return 0;
+}
